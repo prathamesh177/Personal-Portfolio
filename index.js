0b1840ca718448899c8394fc2e3fe5209c046b7e
prathamesh177 <121346957+prathamesh177@users.noreply.github.com>
2024-06-25 12:46:06 +0530

    Add files via upload
    Website live at :- https://prathamesh-walvekar.netlify.app/

---
+++ index.js
@@ -0,0 +1,63 @@
+// const rectangle = {
+//     length:1,
+//     breadth:2,
+//     draw:function(){
+//         console.log("hello world");
+//     }
+    
+// };
+
+// factoray function
+
+// function createRectangle(len,br){
+//     return rectangle={
+//         length:len,
+//     breadth:br,
+//     draw(){
+//         console.log("hello world");
+//     }
+//     }
+// }
+
+// let reactanObj1=createRectangle(5,3);
+
+
+//constructor function
+
+// function rectangle(){
+//     this.length =1;
+//     this.breadth=2;
+
+// }
+// let obj1=new rectangle();
+// obj1.color="yellow";
+
+// delete obj1.color;
+
+//template literal
+
+// let message = `
+// hello
+// i am prathamesh
+// 20 complete `
+
+
+// let date = new Date();
+// console.log(date);
+
+// let date2 = new Date('April 17 2003');
+// console.log(date2);
+
+// let arr=[1,2,3,4]
+// arr.push(6)
+// arr.unshift(3)
+// arr.slice(5,6)
+
+// spread operator
+//  let first = [1,2,3,4]
+//  let sec=[5,6,7,8]
+
+//  let combine = [...first,...sec]
+ 
+
+
